9bc08edae12918ddcc6a13f6a9612c4477c3830d
s14333591 <ictconnecter.m.arakaki@gmail.com>
2022-08-19 00:11:30 +0900

    IDトークンの表示6

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,38 @@ import type { Liff } from "@line/liff";
 import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import { useState, useEffect } from "react";
 
-const Home: NextPage<{ liff: Liff | null; liffError: string | null; idToken: string | null}> = ({
-  liff,
-  liffError,
-  idToken
-}) => {
+// const Home: NextPage<{ liff: Liff | null; liffError: string | null; idToken: string | null}> = ({
+//   liff,
+//   liffError,
+//   idToken
+// }) => {
+const Home: NextPage = () => {
+  const [liffObject, setLiffObject] = useState<Liff | null>(null);
+  const [liffError, setLiffError] = useState<string | null>(null);
+  const [idToken, setIdToken] = useState<string | null>(null);
+
+  // Execute liff.init() when the app is initialized
+  useEffect(() => {
+    // to avoid `window is not defined` error
+    import("@line/liff")
+      .then((liff) => liff.default)
+      .then((liff) => {
+        console.log("LIFF init...");
+        liff
+          .init({ liffId: "1657398058-zj1emPvP" })
+          .then(() => {
+            console.log("LIFF init succeeded.");
+            setLiffObject(liff);
+            setIdToken(liff.getIDToken());
+          })
+          .catch((error: Error) => {
+            console.log("LIFF init failed.");
+            setLiffError(error.toString());
+          });
+      });
+  }, []);
 
   return (
     <div>
@@ -19,7 +45,12 @@ const Home: NextPage<{ liff: Liff | null; liffError: string | null; idToken: str
 
       <main className={styles.main}>
         <h1>create-liff-app</h1>
-        {liff && <><p>LIFF init succeeded.</p><p>IDトークン:{idToken}</p></>}
+        {liffObject && (
+          <>
+            <p>LIFF init succeeded.</p>
+            <p>IDトークン:{idToken}</p>
+          </>
+        )}
         {liffError && (
           <>
             <p>LIFF init failed.</p>
